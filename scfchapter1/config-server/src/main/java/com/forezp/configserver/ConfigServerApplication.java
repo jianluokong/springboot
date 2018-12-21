4fb2e338cb5619769a001abeb7c483216d3059a7
jianluokong <jianluokong@qq.com>
2018-12-21 11:03:54 +0800

    code

--- scfchapter1/config-server/src/main/java/com/forezp/configserver/ConfigServerApplication.java
+++ scfchapter1/config-server/src/main/java/com/forezp/configserver/ConfigServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
+    //http://localhost:8888/foo/dev
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
